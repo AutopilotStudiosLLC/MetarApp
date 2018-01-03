5b2cc37b7b91168767d6d34d8413c3aa46301d4c
Scott Henscheid <scott.henscheid@ritterim.com>
2018-01-03 07:08:31 -0700

    Update the Home Page with actual content.

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import {Station} from "../../models/station.model";
+import {MetarDetailsPage} from "../metar/metarDetails/metarDetails";
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+	metarDetailsPage = MetarDetailsPage;
+
+	favorites: Station[] = [
+		new Station('KMAN')
+	];
+
 
   constructor(public navCtrl: NavController) {
 
