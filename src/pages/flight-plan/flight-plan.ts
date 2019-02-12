3d0aecb94d682b76baa37e7f21deade5911e37b8
Ironpilot <contact@ironpilot.net>
2019-02-11 21:52:24 -0700

    Allow Reorder of Flight Plan Stations and Update on Load.

--- src/pages/flight-plan/flight-plan.ts
+++ src/pages/flight-plan/flight-plan.ts
@@ -4,6 +4,8 @@ import {FlightPlanService} from "../../services/flight-plan.service";
 import {Utility} from "../../models/utility.model";
 import {AddsService} from "../../services/adds.service";
 import {StationService} from "../../services/station.service";
+import {Station} from "../../models/station.model";
+import {Metar} from "../../models/metar.model";
 
 /**
  * Generated class for the FlightPlanPage page.
@@ -27,6 +29,49 @@ export class FlightPlanPage {
 	constructor(public navCtrl: NavController, public navParams: NavParams, private stationService: StationService,
 				private flightPlanService: FlightPlanService, private addsService: AddsService,
 				private loadingCtrl: LoadingController, private alertCtrl: AlertController) {
+	}
+
+	ionViewWillEnter() {
+		let stationsToUpdate:string[] = [];
+		let flightPlanStations = this.flightPlanService.getStations();
+		flightPlanStations.forEach((station) => {
+			if(station.latitude == undefined || station.longitude == undefined) {
+				stationsToUpdate.push(station.ident);
+			}
+		});
+
+		if(stationsToUpdate.length > 0) {
+			this.addsService.getStationsFromList(stationsToUpdate.join(','))
+				.subscribe((stations: Station[]) => {
+					stations.forEach((station) => {
+						let stationToUpdate = flightPlanStations.find((st) => st.ident === station.ident);
+						if (stationToUpdate) {
+							stationToUpdate.latitude = station.latitude;
+							stationToUpdate.longitude = station.longitude;
+							stationToUpdate.elevation = station.elevation;
+							stationToUpdate.name = station.name;
+							stationToUpdate.state = station.state;
+							stationToUpdate.country = station.country;
+							stationToUpdate.isMetarSupported = !!station.isMetarSupported;
+							stationToUpdate.isTafSupported = !!station.isTafSupported;
+						}
+					})
+				});
+		}
+
+		let stationIdentList:string[] = [];
+		flightPlanStations.forEach((station) => {
+			stationIdentList.push(station.ident);
+		});
+		if(stationIdentList.length > 0) {
+			this.addsService.getMetarsFromStationList(stationIdentList.join(','))
+				.subscribe((metars: Metar[]) => {
+					metars.forEach((metar) => {
+						const station = this.flightPlanService.getStation(metar.ident);
+						station.addMetar(metar);
+					});
+				})
+		}
 	}
 
 	onStationSearch(ident: string) {
@@ -56,6 +101,10 @@ export class FlightPlanPage {
 
 	onInputText() {
 		this.stationString = this.stationString.toUpperCase();
+	}
+
+	reorderFlightPlanStations(indexes) {
+		this.flightPlanService.reorderStations(indexes);
 	}
 
 }
