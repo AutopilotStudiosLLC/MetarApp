a3e24139ca27793d00f0913e39e00f189a1d7f93
Ironpilot <contact@ironpilot.net>
2019-08-03 19:15:17 -0600

    Bump the build number.

--- src/services/constants.ts
+++ src/services/constants.ts
@@ -1,4 +1,4 @@
-export const BUILD_VERSION ='0.20190803.1';
+export const BUILD_VERSION ='1.0.190803.1';
 export const GOOGLE_MAPS_KEY = {
 		"apiKey": 'AIzaSyA4-GoZzOqYTvxMe52YQZch5JaCFN6ACLg'
 	};
