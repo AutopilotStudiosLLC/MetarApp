b8fb322cd9975fb4a62a9249a05d930f8f40c542
Alex Mitchell <alex@alappsis.com>
2019-08-14 21:14:33 -0600

    Finished taf content conversions

--- src/components/taf-forecast-content/taf-forecast-content.ts
+++ src/components/taf-forecast-content/taf-forecast-content.ts
@@ -14,9 +14,33 @@ export class TafForecastContent {
 		
 	 }
 
-	forecastFromTime(): string {
-		if(this.forecast != null && this.forecast.fromTime != null){
-			return this.conversionService.convertTimeToConfigured(this.forecast.fromTime);
+	 forecastVisibility(): string {
+        if(this.forecast != null && this.forecast.visibility != null){
+            return this.conversionService.convertStatueMilesToConfigured(Number(this.forecast.visibility)).measurementAndUnit;
+        }
+    }
+
+    forecastWindSpeedMeasurement(): string {
+		if(this.forecast != null && this.forecast.windSpeed != null){
+			return this.conversionService.convertKnotsToConfigured(Number(this.forecast.windSpeed)).measurement;
 		}
 	}
+
+	forecastWindSpeedUnit(): string {
+		if(this.forecast != null && this.forecast.windSpeed != null){
+			return this.conversionService.convertKnotsToConfigured(Number(this.forecast.windSpeed)).unit;
+		}
+    }
+
+    forecastWindGustsMeasurement(): string {
+		if(this.forecast != null && this.forecast.windGusts != null){
+			return this.conversionService.convertKnotsToConfigured(Number(this.forecast.windGusts)).measurement;
+		}
+	}
+
+	forecastWindGustsUnit(): string {
+		if(this.forecast != null && this.forecast.windGusts != null){
+			return this.conversionService.convertKnotsToConfigured(Number(this.forecast.windGusts)).unit;
+		}
+    }
 }
--- src/components/taf-list-item/taf-list-item.module.ts
+++ src/components/taf-list-item/taf-list-item.module.ts
@@ -2,6 +2,7 @@ import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { TafListItem } from './taf-list-item';
 import { TafForecastHeaderModule } from '../taf-forecast-header/taf-forecast-header.module'
+import { TafForecastContentModule } from '../taf-forecast-content/taf-forecast-content.module'
 
 @NgModule({
 	declarations: [
@@ -9,7 +10,8 @@ import { TafForecastHeaderModule } from '../taf-forecast-header/taf-forecast-hea
 	],
 	imports: [
 		IonicPageModule.forChild(TafListItem),
-		TafForecastHeaderModule
+		TafForecastHeaderModule,
+		TafForecastContentModule
 	],
 	exports: [
 		TafListItem
