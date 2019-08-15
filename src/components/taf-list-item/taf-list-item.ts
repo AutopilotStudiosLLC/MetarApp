7a8fc0b45a16b0f530c6f565d8e559a519c12960
Alex Mitchell <alex@alappsis.com>
2019-08-14 21:48:46 -0600

    Added conversions for time and distance on taf list

--- src/components/taf-list-item/taf-list-item.ts
+++ src/components/taf-list-item/taf-list-item.ts
@@ -2,6 +2,7 @@ import {Component, Input} from '@angular/core';
 import {Station} from "../../models/station.model";
 import {Taf} from "../../models/taf.model";
 import {Moment} from "moment";
+import {ConversionService} from "../../services/conversion.service";
 
 @Component({
 	selector: 'taf-list-item',
@@ -11,7 +12,19 @@ export class TafListItem {
 	@Input() station: Station;
 	@Input() taf: Taf;
 
-	constructor() { }
+	constructor(private conversionService: ConversionService) { }
+
+	tafIssueTime(){
+		if(this.taf != null && this.taf.issueTime != null){
+			return this.conversionService.convertTimeToConfigured(this.taf.issueTime);
+		}
+	}
+
+	stationDistance(){
+		if(this.station != null && this.station.getDistance()){
+			return this.conversionService.convertStatueMilesToConfigured(this.station.getDistanceInStatuteMiles(1)).measurementAndUnit;
+		}
+	}
 
 	sameDay(fromTime:Moment, toTime:Moment) {
 		return fromTime.local().format('MMM D') === toTime.local().format("MMM D");
