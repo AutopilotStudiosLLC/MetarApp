872dc32fb8e4b0bf5e7b1a7549fd62897b910e95
Ironpilot <contact@ironpilot.net>
2019-12-04 09:15:40 -0700

    Bump the version in the constants.

--- src/services/constants.ts
+++ src/services/constants.ts
@@ -1,4 +1,4 @@
-export const BUILD_VERSION ='1.0.190923.01';
+export const BUILD_VERSION ='1.0.191203.01';
 export const GOOGLE_MAPS_KEY = {
 		"apiKey": 'AIzaSyA4-GoZzOqYTvxMe52YQZch5JaCFN6ACLg'
 	};
